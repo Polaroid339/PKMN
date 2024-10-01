12885849dcaf804e492bd10b9ac7219e2a6fa5db
Victor Oliveira <168303922+Polaroid339@users.noreply.github.com>
2024-10-01 15:01:46 -0300

    Update pkdex.py

--- pkdex.py
+++ pkdex.py
@@ -4,22 +4,22 @@ p1 = ["Bulbasaur", 45, 49, 49, 65, 65, 45, 100, False]
 p2 = ["Ivysaur", 60, 62, 63, 80, 80, 60, 1, False]
 p3 = ["Venusaur", 80, 82, 83, 100, 100, 80, 1, False]
 
-currentp = p1
 
-statstot = sum(currentp[1:7])
-nota = sum(currentp[1:7]) // len(currentp[1:7])
+def stats(pkmn):
+    print(f"""
+Nome: {pkmn[0]}
+HP: {pkmn[1] + pkmn[7]}
+Attack: {pkmn[2] + pkmn[7]}
+Defense: {pkmn[3] + pkmn[7]}
+Sp Atk: {pkmn[4] + pkmn[7]}
+Sp Def: {pkmn[5] + pkmn[7]}
+Speed: {pkmn[6] + pkmn[7]}
+Total: {sum(pkmn[1:7])}
 
-print(f"""
-Nome: {currentp[0]}
-HP: {currentp[1]+currentp[7]}
-Attack: {currentp[2]+currentp[7]}
-Defense: {currentp[3]+currentp[7]}
-Spe Att: {currentp[4]+currentp[7]}
-Spe Def: {currentp[5]+currentp[7]}
-Speed: {currentp[6]+currentp[7]}
-Total: {statstot}
-
-Level: {currentp[7]}
-Nota: {nota}
-Shiny: {currentp[8]}
+Level: {pkmn[7]}
+Nota: {sum(pkmn[1:7]) // len(pkmn[1:7])}
+Shiny: {pkmn[8]}
 """)
+
+
+stats(p1)
